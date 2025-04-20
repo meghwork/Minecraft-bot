6afb288972d19ff2b68e91b764cde3ff47c5a2ff
meghwork <megh.studyy@gmail.com>
2025-04-20 13:06:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const mineflayer = require('mineflayer');
-const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
 const express = require('express');
 const app = express();
 
@@ -12,35 +11,12 @@ function createBot() {
     host: 'BeastSMP-java.aternos.me',
     port: 17030,
     username: 'Binod_op',
-    version: '1.12' // You can use false if you want auto-detect
+    version: '1.12' // Or false for auto
   });
 
-  bot.loadPlugin(pathfinder);
-
   bot.once('spawn', () => {
-    console.log('Bot joined the server!');
-    const mcData = require('minecraft-data')(bot.version);
-    const defaultMove = new Movements(bot, mcData);
-    bot.pathfinder.setMovements(defaultMove);
-
-    // Commands
-    bot.on('chat', (username, message) => {
-      if (username === bot.username) return;
-
-      if (message === 'follow me') {
-        const target = bot.players[username]?.entity;
-        if (!target) return bot.chat("I can't see you!");
-
-        bot.chat(`Following ${username}`);
-        const goal = new goals.GoalFollow(target, 1);
-        bot.pathfinder.setGoal(goal, true);
-      }
-
-      if (message === 'stop') {
-        bot.chat('Okay, I stopped.');
-        bot.pathfinder.setGoal(null);
-      }
-    });
+    console.log('AFK bot joined the server');
+    startRandomActions(bot);
   });
 
   bot.on('end', () => {
@@ -51,6 +27,23 @@ function createBot() {
   bot.on('error', err => {
     console.log('Bot error:', err);
   });
+}
+
+function startRandomActions(bot) {
+  const movements = ['forward', 'back', 'left', 'right'];
+
+  setInterval(() => {
+    // Random movement
+    movements.forEach(move => {
+      bot.setControlState(move, Math.random() < 0.5);
+    });
+
+    // Random jump
+    bot.setControlState('jump', Math.random() < 0.3);
+
+    // Random sneak
+    bot.setControlState('sneak', Math.random() < 0.3);
+  }, 2000); // every 2 seconds
 }
 
 createBot();
