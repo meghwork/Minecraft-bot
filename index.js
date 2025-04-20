b34a0579a227d22cb1f82cac9ad8ff6cf55ca237
meghwork <megh.studyy@gmail.com>
2025-04-20 12:51:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
 const express = require('express');
 const app = express();
 
+// Keep-alive web server
 app.get('/', (req, res) => res.send('AFK bot is alive!'));
 app.listen(3000, () => console.log('Web server running on port 3000'));
 
@@ -11,34 +12,32 @@ function createBot() {
     host: 'BeastSMP-java.aternos.me',
     port: 17030,
     username: 'Binod_op',
-    version: '1.12'
+    version: '1.12' // You can use false if you want auto-detect
   });
 
   bot.loadPlugin(pathfinder);
 
   bot.once('spawn', () => {
-    console.log('AFK bot joined Aternos server');
-
+    console.log('Bot joined the server!');
     const mcData = require('minecraft-data')(bot.version);
     const defaultMove = new Movements(bot, mcData);
     bot.pathfinder.setMovements(defaultMove);
 
+    // Commands
     bot.on('chat', (username, message) => {
       if (username === bot.username) return;
 
       if (message === 'follow me') {
-        const player = bot.players[username]?.entity;
-        if (player) {
-          bot.chat(`Following ${username}`);
-          const goal = new goals.GoalFollow(player, 1);
-          bot.pathfinder.setGoal(goal, true);
-        } else {
-          bot.chat("Can't see you!");
-        }
+        const target = bot.players[username]?.entity;
+        if (!target) return bot.chat("I can't see you!");
+
+        bot.chat(`Following ${username}`);
+        const goal = new goals.GoalFollow(target, 1);
+        bot.pathfinder.setGoal(goal, true);
       }
 
       if (message === 'stop') {
-        bot.chat('Okay, stopping.');
+        bot.chat('Okay, I stopped.');
         bot.pathfinder.setGoal(null);
       }
     });
