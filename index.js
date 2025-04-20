48a78f3bbc3fe5dee613ad873ede31a811f2e8e0
meghwork <megh.studyy@gmail.com>
2025-04-20 12:36:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 const mineflayer = require('mineflayer');
+const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
 const express = require('express');
 const app = express();
 
-// Web server to keep Replit alive
 app.get('/', (req, res) => res.send('AFK bot is alive!'));
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Web server running on port ${PORT}`);
-});
+app.listen(3000, () => console.log('Web server running on port 3000'));
 
 function createBot() {
   const bot = mineflayer.createBot({
     host: 'BeastSMP-java.aternos.me',
     port: 17030,
-    username: 'Binod_op',
+    username: 'bot_',
     version: '1.12'
   });
 
+  bot.loadPlugin(pathfinder);
+
   bot.once('spawn', () => {
     console.log('AFK bot joined Aternos server');
-    startRandomActions(bot);
+
+    const mcData = require('minecraft-data')(bot.version);
+    const defaultMove = new Movements(bot, mcData);
+    bot.pathfinder.setMovements(defaultMove);
+
+    bot.on('chat', (username, message) => {
+      if (username === bot.username) return;
+
+      if (message === 'follow me') {
+        const player = bot.players[username]?.entity;
+        if (player) {
+          bot.chat(`Following ${username}`);
+          const goal = new goals.GoalFollow(player, 1);
+          bot.pathfinder.setGoal(goal, true);
+        } else {
+          bot.chat("Can't see you!");
+        }
+      }
+
+      if (message === 'stop') {
+        bot.chat('Okay, stopping.');
+        bot.pathfinder.setGoal(null);
+      }
+    });
   });
 
   bot.on('end', () => {
@@ -30,23 +52,6 @@ function createBot() {
   bot.on('error', err => {
     console.log('Bot error:', err);
   });
-}
-
-function startRandomActions(bot) {
-  const movements = ['forward', 'back', 'left', 'right'];
-
-  setInterval(() => {
-    // Random movement
-    movements.forEach(move => {
-      bot.setControlState(move, Math.random() < 0.5);
-    });
-
-    // Random jump
-    bot.setControlState('jump', Math.random() < 0.3);
-
-    // Random sneak
-    bot.setControlState('sneak', Math.random() < 0.3);
-  }, 2000); // change behavior every 2 seconds
 }
 
 createBot();
